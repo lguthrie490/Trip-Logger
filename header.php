ed164a8b821f32fec573e8c5923c43fbabb9d79f
Logan Guthrie <lguthrie490@gmail.com>
2016-11-07 13:32:12 -0800

    Adds bootstrap navbar

--- header.php
+++ header.php
@@ -8,3 +8,10 @@
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
+  <nav class="navbar navbar-default">
+    <ul class="nav navbar-nav">
+      <li><a href="/read.php">Read</a></li>
+      <!--<li><a href="/trip.php">Insert Trip</a></li>-->
+      <li><a href="/index.php">Insert Vehicle</a></li>
+    </ul>
+  </nav>
