a310229dbb7b31f9ee0b316ee693f60b097313ef
Logan Guthrie <lguthrie490@gmail.com>
2016-11-07 13:23:39 -0800

    Makes Read function work

--- read.php
+++ read.php
@@ -4,22 +4,19 @@ include_once 'header.php';
 include_once 'config/Database.php';
 $database = new Database();
 $db = $database->connect();
-//
-// echo "<div class='right-button-margin'>";
-//     echo "<a href='create_product.php' class='btn btn-default pull-right'>Create Product</a>";
-// echo "</div>";
+
 include_once 'objects/Vehicles.php';
 $vehicle = new Vehicle($db);
 
 $stmt = $vehicle->read();
 $num = $stmt->rowCount();
-
 if($num > 0){
-    echo "<table class='table table-responsive>";
+    echo "<table class='table table-responsive'>";
       echo "<tr>";
+        echo "<td>ID</td>";
         echo "<td>Name</td>";
         echo "<td>Vehicle</td>";
-        echo "</tr>";
+      echo "</tr>";
 
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
       extract($row);
