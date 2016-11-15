5a1a9a55e2a066add9426e6b991062a820077463
Logan Guthrie <lguthrie490@gmail.com>
2016-11-15 15:46:22 -0800

    Adds onload function for body. Needs to be changed for non-reading pages

--- header.php
+++ header.php
@@ -4,10 +4,12 @@
     <meta charset="utf-8">
     <?php $pagename = "OOP"; ?>
     <title><?php echo $pagename; ?></title>
-    <link rel="stylesheet" href="style.css" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+    <script type="text/javascript" src="config/xml.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBTksDmo7svHYn-SnzD0dLKsj_VPlIqQQc" type="text/javascript" async defer></script>
+    <link rel="stylesheet" href="style.css" media="screen" title="no title">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title">
   </head>
-  <body>
+  <body onload="load()">
   <nav class="navbar navbar-default">
     <ul class="nav navbar-nav">
       <li><a href="/read.php">Read</a></li>
